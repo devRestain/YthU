9bebb1dd4aef0ddee96d0c395cad015e5101f2f0
devRestain <rhaql1020@snu.ac.kr>
2024-08-30 02:52:53 +0900

    Trying to generate the first chain

--- api/llm/chain.py
+++ api/llm/chain.py
@@ -1,13 +1,60 @@
 from langchain_core.output_parsers.pydantic import PydanticOutputParser
-from api.utils.model_schema import Conversation, Treatment
-from api.llm.prompt import script_classifier_prompt
+from langchain_core.runnables import RunnableLambda
+from langchain_openai.llms import OpenAI
+from api.utils.model_schema import Conversation, Script, Treatment
+from api.llm.prompt import (
+    script_classifier_prompt,
+    md_formatter_prompt,
+    conversation_classifier_prompt,
+)
+
+llm = OpenAI(temperature=0.1)
+
 
 # chain 1. 녹취 스크립트를 과거력 파일로 변환
-# llm 1. 스크립트를 두 세력의 대화로 분류
-conv_parser = PydanticOutputParser(pydantic_object=Conversation)
-conv_prompt = script_classifier_prompt.partial(
-    format_instructions=conv_parser.get_format_instructions()
-)
-# llm 2. 대화 파일을 보고 정해진 카테고리에 대화 내용을 삽입 - pydantic bind
-tx_parser = PydanticOutputParser(pydantic_object=Treatment)
-# llm 3. 각 카테고리에 삽입된 대화 내용을 전문적인 용어로 편집
+def convert_record_to_tx(raw_file: str) -> Treatment:
+
+    # llm 1. 스크립트를 두 세력의 대화로 분류
+    def generate_script(raw_file: str) -> Script:
+        conv_parser = PydanticOutputParser(pydantic_object=Script)
+        conv_prompt = script_classifier_prompt.partial(
+            format_instructions=conv_parser.get_format_instructions()
+        )
+        chain_gen_script = conv_prompt | llm | conv_parser
+        return chain_gen_script.invoke({"input": raw_file})
+
+    # llm 2. 각 카테고리에 삽입된 대화 내용을 전문적인 용어로 편집
+    def format_by_md(script: Script) -> Script:
+        chain_format_by_md = md_formatter_prompt | llm
+        return Script(
+            [
+                Conversation(
+                    {
+                        "content": chain_format_by_md.invoke(
+                            {"input": conv.content}
+                        ).content,
+                        "person": conv.person,
+                    }
+                )
+                for conv in script
+            ]
+        )
+
+    # llm 3. 대화 파일을 보고 정해진 카테고리에 대화 내용을 삽입 - pydantic bind
+    def generate_treatment(script: Script) -> Treatment:
+        tx_parser = PydanticOutputParser(pydantic_object=Treatment)
+        tx_prompt = conversation_classifier_prompt.partial(
+            format_instructions=tx_parser.get_format_instructions()
+        )
+        chain_gen_tx = tx_prompt | llm | tx_parser
+        return chain_gen_tx.invoke({"input": script})
+
+    # llm 4. 대화 파일에서 추가할 수 있는 사항 없는지 재확인.
+
+    # final chain 생성
+    chain_num_first = (
+        RunnableLambda(generate_script)
+        | RunnableLambda(format_by_md)
+        | RunnableLambda(generate_treatment)
+    )
+    return chain_num_first.invoke({"raw_file": raw_file})
--- api/llm/prompt.py
+++ api/llm/prompt.py
@@ -2,15 +2,41 @@ from langchain_core.prompts import PromptTemplate
 
 # 프롬프트 엔지니어링
 pydantic_parse = """Wrap the output in `json` tags\n{format_instructions}"""
-give_motivation = """REMEMBER. You are serving a very important customer.
+give_motivation = """REMEMBER. You are serving a very important client.
 If you produce satisfactory results, you will receive a corresponding bonus, but if you don't, you will be fired."""
-start_work = """Now do your job to the BEST of your ability.\n<<INPUT>>: {input}"""
+start_work = """Now do your job to the BEST of your ability.\n<<INPUT>>:\n{input}"""
 
 # chain 1.
-script_classifier = """"""
+# llm 1.
+script_classifier = """You are an office work expert employed by a large company that cannot be named.
+Your job is to classify who said the sentence when the patient and the doctor received the conversation without distinction.
+Find one utterance in the text line, and distinguish between the patient and the doctor."""
 
 script_classifier_prompt = PromptTemplate.from_template(
     script_classifier
+    + "\n"
+    + pydantic_parse
+    + "\n"
+    + give_motivation
+    + "\n"
+    + start_work
+)
+# llm 2.
+md_formatter = """You are a medical doctor employed by a large company that cannot be named.
+Your job is to correct the given sentence in strict and precise medical terms.
+Correct the sentence in a concise and professional tone."""
+
+md_formatter_prompt = PromptTemplate.from_template(
+    md_formatter + "\n" + give_motivation + "\n" + start_work
+)
+# llm 3.
+conversation_classifier = """You are a doctor employed by a large company who cannot be named.
+Your job is to categorize the given utterances according to the existing categories.
+The categories include chief complaints, past medical history, diagnosis, and future care plan.
+Check out the scheme below for more information about the categories."""
+
+conversation_classifier_prompt = PromptTemplate.from_template(
+    conversation_classifier
     + "\n"
     + pydantic_parse
     + "\n"
